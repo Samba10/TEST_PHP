f97ee1a4eae2e9e1e6fbea53d4e2a464953aefe4
samba <kasambadiadie@gmail.com>
2018-01-19 12:34:07 +0000

    test

---
+++ travel/travel.php
@@ -0,0 +1,283 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+       <meta http-equiv="X-UA-Compatible" content="ie=edge">
+       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
+       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
+           <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+           <script charset="utf-8" src="http://code.jquery.com/jquery-1.6.1.min.js"></script>
+           <script charset="utf-8" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script>
+           <script charset="utf-8" src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.6.1.min.js"></script>
+    <title>Mon site de JUMIA en HTML</title>
+
+    <style type="text/css">
+    .article7 div a{
+        color: grey;
+        font-size: 14px;
+    }
+    .foot1 div a{
+        color: white;
+        font-size: 14px;
+    }
+    .foot1 div a:hover{
+        text-decoration: none;
+    }
+    </style>
+    
+
+</head>
+<body>
+	
+<div class="container">
+
+            <article class="article0" style="text-align: center; color: grey">
+            <div class="row">
+                 <div class="col-6 col-sm-12" style="background-color:white;">
+                     <h2>Meilleurs Destinations</h2>
+                 </div>
+            </div>
+        </article>
+
+        <article class="article1">
+             <div class="row">
+                <div class="col d-none d-sm-block col-sm-3">
+                    <a href=""><strong style="z-index: 2; position: absolute; color: white; margin-left: 90px; margin-top: 70px; font-size: 30px">Dakar</strong></a>
+                   <a href=""><img src="16.jpg" alt="Dakar" style="width: 270px; height: 190px"></a>
+               </div>
+                <div class="col d-none d-sm-block col-sm-3">
+                    <a href=""><strong style="z-index: 2; position: absolute; color: white; margin-left: 90px; margin-top: 70px; font-size: 30px">Saly</strong></a>
+                    <a href=""><img src="18.jpg" alt="Saly" style="width: 270px; height: 190px"></a> 
+                </div>
+                <div class="col d-none d-sm-block col-sm-3">
+                    <a href=""><strong style="z-index: 2; position: absolute; color: white; margin-left: 90px; margin-top: 70px; font-size: 30px">Saint Louis</strong></a>
+                    <a href=""><img src="21.jpg" alt="Saint Louis" style="width: 270px; height: 190px"></a> 
+                </div>
+                <div class="col d-none d-sm-block col-sm-3">
+                    <a href=""><strong style="z-index: 2; position: absolute; color: white; margin-left: 80px; margin-top: 70px; font-size: 30px">Brazzaville</strong></a>
+                   <a href=""><img src="29.jpg" alt="Brazzaville" style="width: 270px; height: 190px"></a> 
+                </div>
+            </div>
+        </article>
+
+   	    <article class="article2">
+    		<div class="row">
+                 <div class="col-6 col-sm-3">
+                    <br><a href=""><strong style="z-index: 2; position: absolute; color: white; margin-left: 90px; margin-top: 70px; font-size: 30px">Abidjan</strong></a>
+                    <a href=""><img src="29.jpg" alt="Abidjan" style="width: 270px; height: 190px"></a> 
+                </div>
+                <div class="col d-none d-sm-block col-sm-3">
+                    <br><a href=""><strong style="z-index: 2; position: absolute; color: white; margin-left: 90px; margin-top: 70px; font-size: 30px">Douala</strong></a>
+                    <a href=""><img src="9.jpg" alt="Douala" style="width: 270px; height: 190px"></a> 
+                </div>
+                <div class="col d-none d-sm-block col-sm-3">
+                    <br><a href=""><strong style="z-index: 2; position: absolute; color: white; margin-left: 90px; margin-top: 70px;font-size: 30px">Lagos</strong></a>
+                   <a href=""><img src="20.jpg" alt="Lagos" style="width: 270px; height: 190px"></a> 
+               </div>
+                <div class="col d-none d-sm-block col-sm-3">
+                    <br><a href=""><strong style="z-index: 2; position: absolute; color: white; margin-left: 80px; margin-top: 70px; font-size: 30px">Diani Beach</strong></a>
+                   <a href=""><img src="12.jpg" alt="Diani Beach" style="width: 270px; height: 190px"></a> 
+               </div>
+            </div>
+        </article>
+
+        <article class="article0" style="text-align: center; color: grey">
+            <div class="row">
+                <div class="col-6 col-sm-12 text-center">
+                    <a class="voirtous" href="">
+                       <div id="voirtous-box" style="color: grey;"><br>Voir toutes les destinations</div>
+                    </a>
+                </div>
+            </div>
+        </article><br>
+
+        <article class="article0" style="text-align: center; color: grey">
+            <div class="row">
+                 <div class="col-6 col-sm-12">
+                     <h2>Découvrer Nos Meilleurs Offres</h2>
+                 </div>
+            </div>
+        </article>
+
+        <article class="article3">
+            <div class="row">
+                 <div class="col-6 col-sm-6">
+                     <img src="1.jpg"><br><br>
+                     <img src="4.jpg">
+                 </div>
+                 <div class="col-6 col-sm-6">
+                     <img src="3.jpg"><br><br>
+                     <img src="2.jpg">
+                 </div>
+            </div>
+        </article>
+
+        <article class="article4">
+            <div class="row">
+                 <div class="col-sm-2">
+                    <br><br><img src="14.png">
+                 </div>
+                 <div class="col-sm-10">
+                   <br><br><br><strong style="font-size: 22px; color: grey">Découvrez nos offres en avant-première ! Souscrivez à notre newsletter maintenant et profitez de nos meilleurs promotions sur vos destinations préférées.</strong><br><br>
+                   <input type="text" style="width: 400px" placeholder="Veuillez indiquer votre email">
+                   <a href="#" class="btn btn-primary">S'inscrire</a>
+                </div>
+            </div>
+        </article>
+
+        <article class="article0" style="text-align: center; color: grey">
+            <div class="row">
+                 <div class="col-6 col-sm-12">
+                     <br><br><h2>Comment ça Marche?</h2><br><br>
+                 </div>
+            </div>
+        </article>
+
+        <article class="article5">
+            <div class="row">
+                 <div class="col-6 col-sm-4" style="background-color: ">
+                    <br><br><img src="31.png"><br><br><h3 style="color: grey">Cherchez</h3>
+                     <p style="color: grey">Entrer votre destination, les date de voyage et le nombre de personnes dans le moteur de recherche</p>
+                 </div>
+                 <div class="col-6 col-sm-4">
+                     <img src="32.png"><br><br><h3 style="color: grey">Comparez</h3>
+                     <p style="color: grey">Trouver votre hébergement idéal au meilleur prix en utilisant le filtre et les optiques de tri (étoiles, prix, localité et équipement).</p>
+                 </div>
+                 <div class="col-sm-4">
+                      <br><img src="33.png"><br><br><h3 style="color: grey">Réservez</h3>
+                      <p style="color: grey">Vous pouvez payer, soit immédiatement en ligne, soit à votre arrivée à l'hotel. Faites vos valises, et partez en toute sérénité !</p>
+                 </div>
+            </div>
+        </article>
+
+        <article class="article0" style="text-align: center; color: grey">
+            <div class="row">
+                 <div class="col-6 col-sm-12"><br><br>
+                     <h2>Ce que disent nos clients et partenaires</h2><br>
+                 </div>
+            </div>
+        </article>
+
+        <article class="article6">
+            <div class="row">
+                <div class="col-6 col-sm-4" style="">
+                    <img src="10.jpg"><br><br>
+                    <strong style="color: grey">Azeez Banks</strong><br><br>
+                    <span style="color: grey">Meme si je vis loin de chez moi, mon coeur est toujours au Nigéria. Je n'ai jamais de problème pour trouver un hébergement lorsque je rentre au pays, Jumia Travel me permet de réserver facilement et sant stress.</span>
+                </div>
+                <div class="col-6 col-sm-4" style="">
+                    <img src="13.jpg"><br><br>
+                    <strong style="color: grey">Isah E. Sule</strong><br><br>
+                    <span style="color: grey">Réserver un hôtel est devenu un plaisir grâce à Jumia Travel et toujours au meilleur prix. Du coup, à chaque fois que je pense voyage, je pense Travel.jumia.com.</span>
+                </div>
+                <div class="col-sm-4" style="">
+                    <img src="17.jpg"><br><br>
+                    <strong style="color: grey">Segun Abiona</strong><br><br>
+                    <span style="color: grey">Super expérience. Réservation et confirmation en quelques minutes ! Bravo Jumia Travel pour votre efficacité !</span>
+                </div>
+            </div>
+        </article>
+
+        <article class="article0" style="text-align: center; color: grey">
+            <div class="row">
+                <div class="col-6 col-sm-12">
+                     <br><br><br><h2>Ils parlent de Jumia Travel</h2><br>
+                </div>
+            </div>
+        </article>
+
+
+         <article class="article0" style="text-align: center; color: grey">
+            <div class="row">
+                 <div class="col-6 col-sm-12">
+                     <img src="7.png">&nbsp;&nbsp;&nbsp;&nbsp;
+                     <img src="15.png">&nbsp;&nbsp;&nbsp;&nbsp;
+                     <img src="rts.png">&nbsp;&nbsp;&nbsp;&nbsp;
+                     <img src="8.png">&nbsp;&nbsp;&nbsp;&nbsp;
+                     <img src="29.png">
+                 </div>
+            </div>
+        </article><hr>
+
+        <article class="article7">
+            <div class="row">
+                    <div class="col-sm-3"><br>
+                        <a href="">Hotels à Dakar</a><br>
+                        <a href="">Hotels Lac Rose</a><br>
+                        <a href="">Hotels à Plateau</a><br>
+                        <a href="">Hotels à Yoff</a><br>
+                        <a href="">Hotels à Ngor</a><br><br>
+                    </div>
+                    <div class="col-sm-3"><br>
+                        <a href="">Hotels à Saint Louis</a><br>
+                        <a href="">Hotels à Cap Skirring</a><br>
+                        <a href="">Hotels à Thiès</a><br>
+                        <a href="">Hotels à Kaolack</a><br>
+                        <a href="">Hotels à Kédougou</a>
+                    </div>
+                    <div class="col-sm-3"><br>
+                        <a href="">Hotels à Saly</a><br>
+                        <a href="">Hotels à Somone</a><br>
+                        <a href="">Hotels à Warang</a><br>
+                        <a href="">Hotels à Mbour</a><br>
+                        <a href="">Hotels à Nianing</a>
+                    </div> 
+                    <div class="col-sm-3"><br>
+                        <a href="">Hotels Neptune Saly</a><br>
+                        <a href="">Ibis Dakar</a><br>
+                        <a href="">Pullman Dakar</a><br>
+                        <a href="">Rhino Resort Saly</a><br>
+                        <a href="">La voile d'or</a>
+                    </div> 
+            </div>
+        </article>
+
+        <footer class="foot1">
+            <div class="row">
+                	<div class="col-sm-3" style="background-color: rgb(26, 26, 78); color: white"><br>Réserver sur Jumia Travel<br><br>
+                        <a href="">FAQ</a><br>
+                        <a href="">Comment ça marche ?</a><br>
+                        <a href="">Contactez-nous</a><br>
+                        <a href="">Codes promotionnels</a><br>
+                        <a href="">Meilleurs Prix Garanti</a>
+                    </div><br>
+                	<div class="col-sm-3" style="background-color: rgb(26, 26, 78);color: white"><br>A propos<br><br>
+                        <a href="">Qui sommes-nous ?</a><br>
+                        <a href="">Rejoignez-nous</a><br>
+                        <a href="">Conditions Générales</a><br>
+                        <a href="">Politique de confidentialité</a><br>
+                        <a href="">Plan du site</a>
+                    </div>
+                    <div class="col-sm-3" style="background-color: rgb(26, 26, 78);color: white"><br>Partenariat & Media<br><br>
+                        <a href="">Inscrivez votre hotel</a><br>
+                        <a href="">Nos Partenaires</a><br>
+                        <a href="">Hospitality Report</a>
+                    </div> 
+                    <div class="col-sm-3" style="background-color: rgb(26, 26, 78);color: white"><br>Suivez-nous<br><br>
+                         <a href=""><img src="26.png" style="width: 150px"></a>
+                    </div> 
+            </div> 
+        </footer>
+
+
+        <footer class="foot2">
+                <div class="row">
+                    <div class="col-sm-9"></div>
+                    <div class="col-sm-3">
+                       <br><span style="color: grey; font-size: 12px">En partenariat avec&nbsp;&nbsp;<img src="11.png"></span>
+                    </div>
+                </div>
+                 <div class="row">
+                    <div class="col-sm-4">
+                        <span style="color: grey; font-size: 12px">Droit d'auteur @ 2018 Jumia Travel. Tous droits réservés.</span>
+                    </div>
+                    <div class="col-sm-8"></div>
+                </div>
+        </footer>
+
+
+      </div>
+   </body>
+</html>
